3fa7e3accad3d21ff5eed3330d5174d814bbbd7e
Ilya <balashovia@bk.ru>
2021-12-15 02:10:59 +0300

    Калькулятор сделан

--- static/js/calculate.js
+++ static/js/calculate.js
@@ -10,7 +10,9 @@ $(document).ready(function () {
       $('#send-calculation').on('click',function(){
           $.ajax({
               data: {
-                  'checked': $('input[name="chk-option"]:checked').serialize(),
+                  'checked_base': $('input[name="chk-base"]:checked').serialize(),
+                  'checked_secondary': $('input[name="chk-secondary"]:checked').serialize(),
+                  'checked_type': $('input[name="chk-type"]:checked').serialize(),
                   'square': $('#square').val(),
                   'csrfmiddlewaretoken': csrf_token
                     },
@@ -19,9 +21,19 @@ $(document).ready(function () {
               success: function (response) {
                   console.log(response.result)
                   $('.calc-result').html(response.result);
-                  if (!$('.button-reset-calc').length){
+                  if (response.flag){
+                      if (!$('.button-reset-calc').length){
                       $('#reset-calc').append($('<button class="button-reset-calc">Сбросить</button>'));
+                      $('.result-note-after').html(
+                          '<div class="result-after-note-text">' +
+                              '<div>Примечание:</div>' +
+                              '<div>1. Расчет яляется предварительным и не является конечным предложением, точная смета составляется только после выполнения проектных работ.</div>' +
+                              '<div>2. Чтобы приблизить предварительный расчет к фактическому рекомендуем заполнить опросный лист и отправить нам или обратиться к нам за консультацией</div>' +
+                          '</div>'
+                          )
+                      }
                   }
+
 
                 }
           });
@@ -32,11 +44,15 @@ $(document).ready(function () {
               url: '/reset_calculation/',
               success: function () {
                   $('.calc-result').html('');
-                  $('input[name="chk-option"]:checked').prop('checked', false);
+                  $('.result-note-after').html('');
+                  $('input[name="chk-base"]:checked').prop('checked', false);
+                  $('input[name="chk-secondary"]:checked').prop('checked', false);
+                  $('input[name="chk-type"]:checked').prop('checked', false);
+                  $('.button-reset-calc').remove()
                 }
           });
       });
-
+ы
 
 
 
