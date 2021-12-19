0177119ce3ceec4057a4da7d17b1c3147bbac098
Ilya <balashovia@bk.ru>
2021-12-20 01:38:03 +0300

    Изменения Portfolioapp

--- settings_local.py
+++ settings_local.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 DEBUG = True
